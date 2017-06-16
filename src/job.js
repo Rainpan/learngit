c36d0bc622d34f2b110b28919c2c7cda4e43c423
panda <624995915@qq.com>
2017-06-16 17:17:54 +0800

    learn change

--- src/job.js
+++ src/job.js
@@ -1,10 +1,3 @@
-var http = require('http');
-
-var EventEmittter = require('events').EventEmitter;
-var event = new EventEmittter();
-event.on('some_event',function () {
-    console.log('事件被触发了');
-});
-setTimeout(function () {
-    event.emit('some_event');
-},1000);
+var buf=new Buffer(10);
+buf.write("hello",'utf8');
+console.info(buf.toJSON());
