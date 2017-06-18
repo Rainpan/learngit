1ec223b33efc85700e1a25e86a7dd131dac1ca5c
tianthj11 <624995915@qq.com>
2017-06-18 21:50:32 +0800

    change something

--- src/hello.js
+++ src/hello.js
@@ -1,3 +1,16 @@
 /**
  * Created by panda on 2017/6/17.
  */
+
+exports.hello = function(){
+    console.log('hello world');
+};
+
+var hello ={
+  'hello':function () {
+      console.log('tianruiqi');
+  },
+    'int':1
+};
+
+module.exports = hello;
--- src/url/start.js
+++ src/url/start.js
@@ -1,3 +1,23 @@
 /**
  * Created by panda on 2017/6/17.
  */
+var http = require('http');
+var url = require('url');
+
+function start(router){
+    function onRequest(request,response){
+        var pathname = url.parse(request.url).pathname;
+        console.log("request for"+pathname+" received");
+
+        router(pathname);
+
+        response.writeHead(200,{"Content-Type":"text/html"});
+        response.write("hello world");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(999);
+    console.log("Server has start");
+}
+
+exports.start = start;
